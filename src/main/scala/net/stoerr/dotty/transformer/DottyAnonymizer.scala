ba4554ee0b5519edd50713267eb32ed38bab1f57
stoerr <KYaLm9tsZbTNcuUhOr8M>
2012-10-08 21:36:50 +0200

    Dotty anonymizer (untested yet)

---
+++ src/main/scala/net/stoerr/dotty/transformer/DottyAnonymizer.scala
@@ -0,0 +1,40 @@
+package net.stoerr.dotty.transformer
+
+import scala.io.Source
+import net.stoerr.dotty.parser.{EdgeDef, NodeDef, DottyGraph, DottyParser}
+import scala.util.parsing.input.CharSequenceReader
+import scala.collection.mutable
+import java.io.{FileOutputStream, OutputStreamWriter}
+
+/**
+ * Removes names from a dotty file such that we can publish it without disclosing
+ * too much information about the project it is from.
+ * @author <a href="http://www.stoerr.net/">Hans-Peter Stoerr</a>
+ * @since 08.10.12
+ */
+object DottyAnonymizer extends DottyParser {
+
+  val randomchars = " llan fair pwll gwyngyll gogerych wyrn drobwl lan tisilio gogo goch"
+
+  def main(args: Array[String]) {
+    val dottyfilecontent = Source.fromFile(args(0)).mkString
+    val dottygraph = dottyfile(new CharSequenceReader(dottyfilecontent)) match {
+      case Success(t, _) => t
+      case NoSuccess(msg, _) => throw new IllegalArgumentException(
+        "Could not parse '" + dottyfilecontent + "': " + msg)
+    }
+    val nodes = dottygraph.edges.map(_.from).toSet ++ dottygraph.edges.map(_.to).toSet
+    val nodemap = new mutable.HashMap[String, String]()
+    var counter = 1
+    for (node <- nodes) {
+      nodemap += node -> ("n" + counter + randomchars.substring(0, node.length - 3))
+      counter += 1
+    }
+    val outgraph = DottyGraph(dottygraph.directed, dottygraph.name, dottygraph.nodes.map(nodedef => NodeDef(nodemap(nodedef.name), nodedef.attrib)),
+      dottygraph.edges.map(edgedef => EdgeDef(nodemap(edgedef.from), nodemap(edgedef.to), edgedef.attrib)))
+    val out = new OutputStreamWriter(new FileOutputStream(args(1)))
+    out.write(outgraph.toString())
+    out.close()
+  }
+
+}
